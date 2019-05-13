f52dd7cf0857770c1cadbeb8084190e968544768
day1park <daypark2408@gmail.com>
2019-05-13 21:07:12 +1000

    random header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <h1>App component</h1>
+      <b>fdkj</b>
+      <Header />
     </div>
   );
 }
