be0354ef7200af3c10edcb626c8fd59116097279
day1park <daypark2408@gmail.com>
2019-05-14 16:41:17 +1000

    create contact component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>App component</h1>
       <b>fdkj</b>
+      <Contact />
       <Header />
     </div>
   );
---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Johnny Bravo</h4>
+        <ul>
+          <li>Email: jbravo@gmail.com</li>
+          <li>Phone: 123-123-123</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;
