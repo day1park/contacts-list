82fe58856e0a61bd3e72b58e1f3075331eac4026
day1park <daypark2408@gmail.com>
2019-05-14 17:55:19 +1000

    yarn add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Header from "./components/Header";
 import Contact from "./components/Contact";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <div className="App">
