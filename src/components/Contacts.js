4c32507853dfaece7e50fd11c8376af1f799fb8c
day1park <daypark2408@gmail.com>
2019-05-15 17:23:09 +1000

    loop through contacts to output singular contact

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,37 +1,36 @@
 import React, { Component } from "react";
+import Contact from "./Contact";
 
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: "Johnny Delta",
-          email: "jdelta@gmail.com",
-          phone: "123-123-123"
-        },
-        {
-          id: 2,
-          name: "Jane Echo",
-          email: "jecho@gmail.com",
-          phone: "123-456-789"
-        },
-        {
-          id: 3,
-          name: "Marvin Gaye",
-          email: "mgaye@gmail.com",
-          phone: "911-911-911"
-        }
-      ]
-    };
-  }
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "Johnny Delta",
+        email: "jdelta@gmail.com",
+        phone: "123-123-123"
+      },
+      {
+        id: 2,
+        name: "Jane Echo",
+        email: "jecho@gmail.com",
+        phone: "123-456-789"
+      },
+      {
+        id: 3,
+        name: "Marvin Gaye",
+        email: "mgaye@gmail.com",
+        phone: "911-911-911"
+      }
+    ]
+  };
+
   render() {
     const { contacts } = this.state;
     return (
       <div>
         {contacts.map(contact => (
-          <h1>{contact.name}</h1>
+          <Contact contact={contact} key={contact.id} />
         ))}
       </div>
     );
