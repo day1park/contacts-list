bf67690c48d35477e1fedbc8e862b9a8e9a234b3
day1park <daypark2408@gmail.com>
2019-05-15 22:01:20 +1000

    add delete function through context consumer dispatch

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-
+import { Consumer } from "../context";
 class Contact extends Component {
   state = {
     showContactInfo: false
@@ -14,34 +14,46 @@ class Contact extends Component {
   };
 
   // deletes contact on click
-  onDeleteClick = () => {};
+  onDeleteClick = (id, dispatch) => {
+    dispatch({
+      type: "DELETE_CONTACT",
+      payload: id
+    });
+  };
 
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { id, name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
 
     return (
-      <div
-        className="card card-body mb-3"
-        style={{ cursor: "pointer" }}
-        onClick={this.onShowClick}
-      >
-        <h4>
-          {name} <i className="fas fa-caret-down" />
-          <i
-            className="fas fa-minus-circle"
-            style={{ cursor: "pointer", float: "right", color: "red" }}
-            onClick={this.onDeleteClick}
-          />
-        </h4>
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div
+              className="card card-body mb-3"
+              style={{ cursor: "pointer" }}
+              onClick={this.onShowClick}
+            >
+              <h4>
+                {name} <i className="fas fa-caret-down" />
+                <i
+                  className="fas fa-minus-circle"
+                  style={{ cursor: "pointer", float: "right", color: "red" }}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                />
+              </h4>
 
-        {showContactInfo ? (
-          <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
-            <li className="list-group-item">Phone: {phone}</li>
-          </ul>
-        ) : null}
-      </div>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
