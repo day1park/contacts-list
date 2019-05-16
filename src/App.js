f623be3d9526c9da3dcdeda807485733a34991a4
day1park <daypark2408@gmail.com>
2019-05-16 18:02:31 +1000

    fix file structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Contacts from "./components/Contacts";
+import Header from "./components/layout/Header";
+import Contacts from "./components/contacts/Contacts";
 import { Provider } from "./context";
 import "bootstrap/dist/css/bootstrap.min.css";
 
