c2dd6e7cc8b2ba24af89fa5594fb3778198be674
day1park <daypark2408@gmail.com>
2019-06-01 23:36:40 +1000

    testing lifecycle methods

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 
 class Test extends Component {
+  componentDidMount() {
+    console.log("component DID mount...");
+  }
+
+  componentWillMount() {
+    console.log("component WILL mount");
+  }
+
+  componentDidUpdate() {
+    console.log("component DID update");
+  }
+
   render() {
     return (
       <div>
