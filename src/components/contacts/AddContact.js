0246a704285793d984a86d1f3e53cedc4b5c6099
day1park <daypark2408@gmail.com>
2019-06-07 04:09:18 +1000

    add axios to addcontact component

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
-import uuid from "uuid";
+// import uuid from "uuid";
 import TextInputGroup from "../layout/TextInputGroup";
+import axios from "axios";
 
 class AddContact extends Component {
   state = {
@@ -33,15 +34,19 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuid(),
       name,
       email,
       phone
     };
-    dispatch({
-      type: "ADD_CONTACT",
-      payload: newContact
-    });
+
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newContact)
+      .then(res =>
+        dispatch({
+          type: "ADD_CONTACT",
+          payload: res.data
+        })
+      );
 
     // Clears state
     this.setState({
